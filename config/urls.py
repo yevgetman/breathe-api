eaf9eb658879e4a30537e2ee7ad93304f2019e10
Yevgeny Getman <yevgetman@gmail.com>
2025-11-07 09:43:15 -0600

    add redirect from root to demo page

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,11 @@ URL Configuration for Air Quality API project.
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import RedirectView
 from apps.api.views import DemoView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/demo/', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.api.urls')),
     path('demo/', DemoView.as_view(), name='demo'),
